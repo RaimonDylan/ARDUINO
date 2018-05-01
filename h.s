5f650743a1aeddfbedd470d00a06469c4e33fc9f
David MOLINARI <contact@davidmolinari.fr>
2018-05-01 14:00:00 +0530

    did some extra calculations,deleted media files,extra code removed

---
+++ h.s
@@ -0,0 +1,24 @@
+#!/bin/bash
+list=("added index file","updated readme file", "did some extra calculations","deleted media files","extra code removed")
+for i in {1..15}
+do
+	touch temp$i.txt
+	git add -A
+	a="Sun may "
+	b=$i
+	c=" 14:00 2018 +0530"
+	export GIT_AUTHOR_DATE=$a$b$c
+	export GIT_COMMITTER_DATE=$a$b$c
+	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
+	rm temp$i.txt
+	export GIT_AUTHOR_DATE=$a$b$c
+	export GIT_COMMITTER_DATE=$a$b$c
+	export GIT_AUTHOR_NAME="David MOLINARI"
+	export GIT_AUTHOR_EMAIL="contact@davidmolinari.fr"
+	export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
+	export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
+	git commit -m "Addes some patches"
+	git commit -am "`echo ${list[$RANDOM % ${#list[@]} ]}`"
+done
+
+
