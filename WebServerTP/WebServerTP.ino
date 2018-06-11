10dee5d2702fe86cd70813d27dd2423b6d31d861
David MOLINARI <contact@davidmolinari.fr>
2018-06-11 16:54:22 +0200

    add graph dynamique

--- WebServerTP/WebServerTP.ino
+++ WebServerTP/WebServerTP.ino
@@ -48,9 +48,12 @@ String cmdHumidity = "";
 String cmdLight = "";
 String cmdVolt = "";
 String cmdBtn = "";
-String valueLight="";
+String valueLight = "";
 int Tmax = 300;
-char CF = '\n'; 
+float tabLight[5] = {0};
+int indexTab = 0;
+char CF = '\n';
+float gros = 0;
 
 ESP8266WebServer server(80);
 SoftwareSerial swSer(D6, D5, false, 256);
@@ -88,7 +91,7 @@ void handleRoot() {
   </body>\
 </html>",
 
-          cmdUse.c_str(),cmdOption.c_str(),cmdTemperature.c_str(),cmdHumidity.c_str(),
+           cmdUse.c_str(), cmdOption.c_str(), cmdTemperature.c_str(), cmdHumidity.c_str(),
            cmdLight.c_str(),
            cmdVolt.c_str(),
            cmdBtn.c_str()
@@ -99,41 +102,38 @@ void handleRoot() {
 
 void handleRoot2() {
   digitalWrite(led, 1);
-  char temp[400];
+  char temp[500];
   int sec = millis() / 1000;
   int min = sec / 60;
   int hr = min / 60;
 
-  snprintf(temp, 400,
+  snprintf(temp, 500,
 
            "<html>\
   <head>\
-    <meta http-equiv='refresh' content='45'/>\
+    <meta charset=\"UTF-8\">\
+    <meta http-equiv='refresh' content='1'/>\
     <title>ESP8266 Demo</title>\
     <style>\
       body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
     </style>\
   </head>\
   <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"./test.svg\" />\
+    <img src=\"/test2.svg\" />\
   </body>\
-</html>",
-
-           hr, min % 60, sec % 60
+</html>"
           );
   server.send(200, "text/html", temp);
   digitalWrite(led, 0);
 }
 
 void aled() {
-  int cmd = (server.arg(0) == "1")?1:0;
+  int cmd = (server.arg(0) == "1") ? 1 : 0;
   digitalWrite(D2, cmd);
-  if(cmd == 1) swSer.write("!G1\n");
-  else if(cmd == 0) swSer.write("!G0\n");
-  
-  
+  if (cmd == 1) swSer.write("!G1\n");
+  else if (cmd == 0) swSer.write("!G0\n");
+
+
 }
 
 void all() {
@@ -163,7 +163,7 @@ void all() {
   </body>\
 </html>",
 
-           cmdUse.c_str(),cmdOption.c_str(),cmdTemperature.c_str(),cmdHumidity.c_str(),
+           cmdUse.c_str(), cmdOption.c_str(), cmdTemperature.c_str(), cmdHumidity.c_str(),
            cmdLight.c_str(),
            cmdVolt.c_str(),
            cmdBtn.c_str()
@@ -192,7 +192,7 @@ void handleNotFound() {
 
 void setup(void) {
   pinMode(led, OUTPUT);
-  pinMode(D2, OUTPUT);  
+  pinMode(D2, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
@@ -219,18 +219,20 @@ void setup(void) {
   }
 
   server.on("/", handleRoot);
+  server.on("/testDynamique", handleRoot2);
   server.on("/test.svg", drawGraphLight);
+  server.on("/test2.svg", drawGraphLightDynamique);
   server.on("/execute", aled);
   server.on("/testall", all);
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works as well");
   });
   SPIFFS.begin();                           // Start the SPI Flash Files System
-  
+
   server.onNotFound([]() {                              // If the client requests any URI
     if (!handleFileRead(server.uri()))                  // send it if it exists
       server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
-});
+  });
   server.begin();
   Serial.println("HTTP server started");
 }
@@ -240,7 +242,7 @@ void loop(void) {
   softSerialEvent();
   if (stringComplete) {
     Serial.println(inputString);
-    getValueAll(inputString,';');
+    getValueAll(inputString, ';');
     rep = inputString;
     // clear the string:
     inputString = "";
@@ -257,34 +259,71 @@ void drawGraphLight() {
   out += "<g stroke=\"black\">\n";
   int tab[42];
   float gros = 0;
-  for(int i=0;i<41;i++){
+  for (int i = 0; i < 41; i++) {
     softSerialEvent();
     float light = atoi(cmdLight.c_str());
     Serial.println(light);
     float calcul = light;
     tab[i] = calcul;
-    gros = (gros < calcul)?calcul:gros;
+    gros = (gros < calcul) ? calcul : gros;
     if (stringComplete) {
-    Serial.println(inputString);
-    getValueAll(inputString,';');
-    rep = inputString;
-    // clear the string:
-    inputString = "";
-    stringComplete = false;
+      Serial.println(inputString);
+      getValueAll(inputString, ';');
+      rep = inputString;
+      // clear the string:
+      inputString = "";
+      stringComplete = false;
+    }
+    delay(1000);
   }
-  delay(1000);
-  }
-  float calcul = tab[0]/gros;
-  calcul = 130*calcul;
+  float calcul = tab[0] / gros;
+  calcul = 130 * calcul;
   int y = calcul;
   int i = 1;
-  
+
   for (int x = 10; x < 390; x += 10) {
-    float calcul = tab[i]/gros;
-  calcul = 130*calcul;
+    float calcul = tab[i] / gros;
+    calcul = 130 * calcul;
     int y2 = calcul;
     Serial.println(y2);
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140-y, x + 10, 140-y2);
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += temp;
+    y = y2;
+    i++;
+  }
+  out += "</g>\n</svg>\n";
+
+  server.send(200, "image/svg+xml", out);
+}
+
+void drawGraphLightDynamique() {
+  
+  String out = "";
+  char temp[100];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"black\">\n";
+  int y = 130;
+  int i = 0;
+  softSerialEvent();
+      tabLight[indexTab] = atoi(cmdLight.c_str());
+      Serial.println(atoi(cmdLight.c_str()));
+      Serial.println(indexTab);
+      Serial.println(tabLight[indexTab]);
+     indexTab++;
+     indexTab = (indexTab == 6)?0:indexTab;
+  for (int x = 10; x < 390; x += 80) {
+    int y2 = 0;
+    gros = (gros < tabLight[i])?tabLight[i]:gros;
+    if(tabLight[i]>0){
+    float calcul = tabLight[i] / gros;
+    calcul = 130 * calcul;
+    y2 = calcul;
+    }else{
+      y2 = 0;
+    }
+    Serial.println(y2);
+    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 80, 140 - y2);
     out += temp;
     y = y2;
     i++;
@@ -295,41 +334,40 @@ void drawGraphLight() {
 }
 
 
-
-
 void softSerialEvent() {
   while (swSer.available()) {
     char inChar = (char)swSer.read();
     // add it to the inputString:
-    if(inputString.length() <= Tmax){
+    if (inputString.length() <= Tmax) {
       if (inChar == CF) {
         stringComplete = true;
-      }else
-         inputString += inChar;
+      } else
+        inputString += inChar;
     }
 
   }
 }
 
+
 void getValueAll(String data, char separator)
 {
   int found = 0;
-  int cpt=0;
-  String value="";
-  int maxIndex = data.length()-1;
-  for(int i=0; i<=maxIndex; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-      if(cpt==0)cmdUse = value;
-      if(cpt==1)cmdOption = value;
-      if(cpt==2)cmdTemperature = value;
-      if(cpt==3)cmdHumidity = value;
-      if(cpt==4)cmdLight = value;
-      if(cpt==5)cmdVolt = value;
-      if(cpt==6)cmdBtn = value;
-      value="";
+  int cpt = 0;
+  String value = "";
+  int maxIndex = data.length() - 1;
+  for (int i = 0; i <= maxIndex; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      if (cpt == 0)cmdUse = value;
+      if (cpt == 1)cmdOption = value;
+      if (cpt == 2)cmdTemperature = value;
+      if (cpt == 3)cmdHumidity = value;
+      if (cpt == 4)cmdLight = value;
+      if (cpt == 5)cmdVolt = value;
+      if (cpt == 6)cmdBtn = value;
+      value = "";
       cpt++;
     }
-    else{
+    else {
       value += data.charAt(i);
     }
   }
@@ -339,16 +377,16 @@ void getValueLight(String data, char separator)
 {
   Serial.println(data);
   int found = 0;
-  int cpt=0;
-  String value="";
-  int maxIndex = data.length()-1;
-  for(int i=0; i<=maxIndex; i++){
-    if(data.charAt(i)==separator || i==maxIndex){
-      if(cpt==1)valueLight = value;
-      value="";
+  int cpt = 0;
+  String value = "";
+  int maxIndex = data.length() - 1;
+  for (int i = 0; i <= maxIndex; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      if (cpt == 1)valueLight = value;
+      value = "";
       cpt++;
     }
-    else{
+    else {
       value += data.charAt(i);
     }
   }
